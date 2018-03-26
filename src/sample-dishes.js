dc3af76e4272c203382e676de3cab1bacc7720a2
thegirlfromipanema <mayanovarini@gmail.com>
2018-03-26 16:47:38 -0700

    first commit

---
+++ src/sample-dishes.js
@@ -0,0 +1,84 @@
+// This is just some sample data so you don't have to think of your own!
+const menu = {
+  dish1: {
+    name: "Muesli & Fresh Fruit",
+    image: "/images/mueli.jpg",
+    desc:
+      "A quick start to the 30 day challenge. Did we mention this was going to be easy? Surprisingly enough there are plenty of commercially available muesli on the market that are also vegan.",
+    price: 13,
+    status: "available"
+  },
+
+  dish2: {
+    name: "Falafel Tabouli Wrap",
+    image: "/images/falafel.jpg",
+    desc:
+      "Falafel can be easily bought at most supermarkets and while you might not be able to always find tabouli there, it is easy to make yourself.",
+    price: 3200,
+    status: "available"
+  },
+
+  dish3: {
+    name: "Banana Berry Smoothie",
+    image: "/images/smoothie.jpg",
+    desc:
+      "Smoothies are always a great fix for when you need to get up and out the door in a hurry.",
+    price: 1684,
+    status: "unavailable"
+  },
+
+  dish4: {
+    name: "Lentil Veggie Burger",
+    image: "/images/burger.jpg",
+    desc:
+      "Everyone loves burgers and with the rise of veganism there are some great alternative meats and plant based patties on the market!",
+    price: 1129,
+    status: "available"
+  },
+
+  dish5: {
+    name: "Chickpea and Veggie Curry",
+    image: "/images/veggiecurry.jpg",
+    desc:
+      "Curries are one of our favourite meals of all times. They are quick and easy, and possibly taste even better the next day.",
+    price: 4234,
+    status: "available"
+  },
+
+  dish6: {
+    name: "Sunflower Fruit Salad",
+    image: "/images/sunflowersalad.jpg",
+    desc:
+      "For an even more colourful start to your day try drizzling a few tablespoons of passionfruit over your fruit salad.",
+    price: 1453,
+    status: "available"
+  },
+
+  dish7: {
+    name: "Tofu Nori Rolls",
+    image: "/images/tofusushi.jpg",
+    desc: "Sushi is a great thing to know how to cook. Perfect for parties, picnics, lunches and work!",
+    price: 2543,
+    status: "available"
+  },
+
+  dish8: {
+    name: "Banana & Flaxseed Weetbix",
+    image: "/images/weetbix.jpg",
+    desc:
+      "This classic Aussie breakfast is easy to veganise with some fortified soy milk.",
+    price: 425,
+    status: "available"
+  },
+
+  dish9: {
+    name: "Chilli Bean Burrito",
+    image: "/images/burrito.jpg",
+    desc:
+      "A burrito is a sleeping bag for quick and easy deliciousness! Pro tip for workers: prepare the fillings the night before in a zip lock bag for maximum freshness.",
+    price: 2250,
+    status: "available"
+  }
+};
+
+export default menu;
